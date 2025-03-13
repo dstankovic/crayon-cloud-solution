6d91352e696c99952a82394362badecc3f923edf
Dusan Stankovic <dstankovic@3mdsolutions.com>
2025-03-13 21:07:43 +0100

    Initial version of CCP service mock

---
+++ CloudSales.Application/Interfaces/ICCPApiService.cs
@@ -0,0 +1,9 @@
+﻿using CloudSales.Application.Models;
+
+namespace CloudSales.Application.Interfaces;
+
+public interface ICCPApiService
+{
+    Task<IEnumerable<SoftwareServiceResponseModel>> GetAvailableSoftwareServicesAsync(CancellationToken cancellationToken);
+    Task OrderLicenseAsync(OrderLicenseRequestModel request, CancellationToken cancellationToken);
+}
---
+++ CloudSales.Infrastructure/Services/CCPApiService.cs
@@ -0,0 +1,20 @@
+﻿using CloudSales.Application.Interfaces;
+using CloudSales.Application.Models;
+
+namespace CloudSales.Infrastructure.Services
+{
+    internal class CCPApiService : ICCPApiService
+    {
+        public async Task<IEnumerable<SoftwareServiceResponseModel>> GetAvailableSoftwareServicesAsync(CancellationToken cancellationToken)
+        {
+            await Task.Delay(300, cancellationToken);
+
+            return Enumerable.Empty<SoftwareServiceResponseModel>();
+        }
+
+        public async Task OrderLicenseAsync(OrderLicenseRequestModel request, CancellationToken cancellationToken)
+        {
+            await Task.Delay(300, cancellationToken);
+        }
+    }
+}
