78adf3e40a2d6b0642e7e7489c9cc0579a07720a
Dusan Stankovic <dstankovic@3mdsolutions.com>
2025-03-14 01:18:19 +0100

    Fixed small errors

---
+++ CloudSales.Api/Validators/UpdateExpirationRequestModelValidator.cs
@@ -0,0 +1,17 @@
+﻿using CloudSales.Application.Models;
+using FluentValidation;
+
+namespace CloudSales.Api.Validators
+{
+    public class UpdateExpirationRequestModelValidator : AbstractValidator<UpdateExpirationRequestModel>
+    {
+        public UpdateExpirationRequestModelValidator()
+        {
+            RuleFor(x => x.ValidTo)
+               .GreaterThan(DateTime.UtcNow)
+               .WithMessage("Valid to must be in the future.");
+        }
+    }
+}
+
+
--- CloudSales.Api/Validators/UpdateQuantityRequestModelValidator.cs
+++ CloudSales.Api/Validators/UpdateQuantityRequestModelValidator.cs
@@ -7,14 +7,6 @@ namespace CloudSales.Api.Validators
     {
         public UpdateQuantityRequestModelValidator()
         {
-            RuleFor(x => x.AccountId)
-               .GreaterThan(0)
-               .WithMessage("Account Id must be greater than 0");
-
-            RuleFor(x => x.SoftwareServiceId)
-                .GreaterThan(0)
-                .WithMessage("SoftwareService Id must be greater than 0");
-
             RuleFor(x => x.Quantity)
                .GreaterThan(0)
                .WithMessage("Quantity must be greater than 0");
